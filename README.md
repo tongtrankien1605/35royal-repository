e52f9f6dc750212ee3d28e2be7888fdee381c104
tongtrankien1605 <tongtrankien1605@gmail.com>
2025-07-10 15:11:48 +0700

    Upload Code Base 35royal-repository on 10/07/2025 by Tống Trần Kiên

---
+++ README.md
@@ -0,0 +1,5 @@
+## 🚀  **Tổng hợp các Repository của Github 35royal** 🚀
+
+<br/>
+
+## 🌐  **👉  Truy Cập [TẠI ĐÂY](https://tongtrankien1605.github.io/35royal-repository) nhé 👈**
---
+++ data.js
@@ -0,0 +1,60 @@
+// Dữ liệu chương trình
+const programs = [
+    {
+        title: "happyhappy",
+        description: "code love của Thương Đàm",
+        url: "https://35royal.github.io/happyhappy"
+    },
+    {
+        title: "phungduyhung",
+        description: "code love của Phùng Duy Hưng",
+        url: "https://35royal.github.io/phungduyhung"
+    },
+    
+    {
+        title: "tongduyhai",
+        description: "code love của Tống Duy Hải",
+        url: "https://35royal.github.io/tongduyhai"
+    },
+    {
+        title: "phamxuanthang",
+        description: "code love của Phạm Xuân Thắng",
+        url: "https://35royal.github.io/phamxuanthang"
+    },
+    {
+        title: "phamngochuyen",
+        description: "code love của Phạm Ngọc Huyền",
+        url: "https://35royal.github.io/phamngochuyen"
+    },
+    {
+        title: "Repo github 35royal",
+        description: "repository 35royal: repo base chứa các tài nguyên",
+        url: "https://github.com/35royal/35royal"
+    },
+    {
+        title: "Autocommit Streak",
+        description: "tự động commit và ghi log thời gian theo các khung giờ chỉ định để giữ chuỗi github",
+        url: "https://github.com/35royal/autocommit-streak"
+    },
+    {
+        title: "Proxy Server cá nhân",
+        description: "Dùng để truy cập các trang web bị chặn. Chạy trên Render. Cần thêm /proxy/ trước link gốc",
+        url: "https://my-proxy-server35.onrender.com"
+    }
+];
+
+// Danh sách link nhạc nền
+const musicList = [
+    "https://cdn.jsdelivr.net/gh/tongtrankien1605/tongtrankien1605@main/global/audio/rapviet/ba-oi-rapviet-robber.mp3",
+    "https://cdn.jsdelivr.net/gh/tongtrankien1605/tongtrankien1605@main/global/audio/rapviet/bao-gio-lay-vo-rapviet-24kright.mp3",
+    "https://cdn.jsdelivr.net/gh/tongtrankien1605/tongtrankien1605@main/global/audio/rapviet/choi-nhu-tui-my-andreerighthand.mp3",
+    "https://cdn.jsdelivr.net/gh/tongtrankien1605/tongtrankien1605@main/global/audio/rapviet/doi-tu-the-remix-binhgold-andreerighthand.mp3",
+    "https://cdn.jsdelivr.net/gh/tongtrankien1605/tongtrankien1605@main/global/audio/rapviet/hoa-hong-gai-rapviet-phapkieu.mp3",
+    "https://cdn.jsdelivr.net/gh/tongtrankien1605/tongtrankien1605@main/global/audio/rapviet/hoi-bi-nghe-rapviet-7dnight.mp3",
+    "https://cdn.jsdelivr.net/gh/tongtrankien1605/tongtrankien1605@main/global/audio/rapviet/hot-shot-rapviet-dlow.mp3",
+    "https://cdn.jsdelivr.net/gh/tongtrankien1605/tongtrankien1605@main/global/audio/rapviet/huynh-kim-long-rapviet-huynhconghieu.mp3",
+    "https://cdn.jsdelivr.net/gh/tongtrankien1605/tongtrankien1605@main/global/audio/rapviet/khi-con-mua-dan-phai-rapviet-tez.mp3",
+    "https://cdn.jsdelivr.net/gh/tongtrankien1605/tongtrankien1605@main/global/audio/rapviet/qua-tung-khung-hinh-rapviet-robber-ngan.mp3",
+    "https://cdn.jsdelivr.net/gh/tongtrankien1605/tongtrankien1605@main/global/audio/rapviet/take-it-easy-rapviet-liugrace.mp3",
+
+];
